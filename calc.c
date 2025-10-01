6d14638eb8775b7ed62f5e15e157b096f23764ec
praman-np <raipraman9@gmail.com>
2025-10-01 22:27:39 +0530

    New.c

--- calc.c
+++ calc.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 int main(){
-    int a;
-    int b;
+    int a,c;
     char op;
-    printf("Enter two numbers:");
+    printf("Enter two no");
     scanf("%d",&a);
-    scanf("%d",&b);
+    printf("Enter a no:");
+    scanf("%d",&c);
     printf("Enter your operator: +,-,*,/:");
     scanf("%s",&op);
     if(op=='+'){
-        int sum=a+b;
-        printf("The sum of two no is:%d %c %d = %d",a,op,b,sum);
+        int sum=a+c;
+        printf("The sum of two no is:%d %c %d = %d",a,op,c,sum);
     }
     else if(op=='-'){
-        int sub=a-b;
-        printf("The difference of two no is:%d %c %d = %d",a,op,b,sub);
+        int sub=a-c;
+        printf("The difference of two no is:%d %c %d = %d",a,op,c,sub);
     }
     else if(op=='*'){
-        int mul=a*b;
-        printf("The multiplication of two no is:%d %c %d=%d",a,op,b,mul);
+        int mul=a*c;
+        printf("The multiplication of two no is:%d %c %d=%d",a,op,c,mul);
     }
     else if(op=='/'){
-        int div=a/b;
-        printf("The quorient of two no is:%d %c %d %d",a,op,b,div);
+        int div=a/c;
+        printf("The quorient of two no is:%d %c %d %d",a,op,c,div);
     }
     else{
         printf("enter a valid operator");
