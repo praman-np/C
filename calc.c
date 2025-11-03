4e6e26a2225b929fc831b349e8011b21ecce1efa
praman-np <raipraman9@gmail.com>
2025-11-03 22:35:27 +0530

    Updated the error

--- calc.c
+++ calc.c
@@ -1,31 +1,33 @@
 #include<stdio.h>
 int main(){
-    int a,c;
-    char op;
+    float num1,num2,res;
+    char optr;
     printf("Enter two no");
-    scanf("%d",&a);
+    scanf("%f",&num1);
+    printf("Enter your operator (+,-,*,/):");
+    scanf(" %c",&optr);
     printf("Enter a no:");
-    scanf("%d",&c);
-    printf("Enter your operator: +,-,*,/:");
-    scanf("%s",&op);
-    if(op=='+'){
-        int sum=a+c;
-        printf("The sum of two no is:%d %c %d = %d",a,op,c,sum);
-    }
-    else if(op=='-'){
-        int sub=a-c;
-        printf("The difference of two no is:%d %c %d = %d",a,op,c,sub);
-    }
-    else if(op=='*'){
-        int mul=a*c;
-        printf("The multiplication of two no is:%d %c %d=%d",a,op,c,mul);
-    }
-    else if(op=='/'){
-        int div=a/c;
-        printf("The quorient of two no is:%d %c %d %d",a,op,c,div);
-    }
-    else{
-        printf("enter a valid operator");
+    scanf("%f",&num2);
+    switch(optr){
+        case '+':
+            res=num1+num2;
+            printf("%f %c %f = %f",num1,optr,num2,res);
+            break;
+        case '-':
+            res=num1-num2;
+            printf("%f %c %f = %f",num1,optr,num2,res);
+            break;
+        case '*':
+            res=num1*num2;
+            printf("%f %c %f = %f",num1,optr,num2,res);
+            break;
+        case '/':
+            res=num1/num2;
+            printf("%f %c %f = %f",num1,optr,num2,res);
+            break;
+        default:
+            printf("Use vailt operator");
+            break;
     }
     return 0;
 }
