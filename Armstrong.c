079bc35425a32cea00ae6ac2fec01cfabdfe81b1
praman-np <raipraman9@gmail.com>
2025-11-09 16:52:58 +0530

    Made it correct

--- Armstrong.c
+++ Armstrong.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
+#include<math.h>
+int armstrong(int num){
+    int temp = num, last, sum=0,length=0;
+    while(temp!=0){
+        temp/=10;
+        length++;
+    }
+    temp = num;
+    while(temp !=0){
+        last = temp % 10;
+        sum +=round((pow(last, length)));
+        temp/=10;
+    }
+    return sum==num;
+    
+}
 int main(){
-    int a,b,c,d=0;
-    printf("Enter a no:");
-    scanf("%d",&a);
-    b=a;
-    while(b!=0){
-        c=b%10;
-        d=d+c*c*c;
-        b=b/10;
-    }
-    if(d==a){
-        printf("Armstrong no");
-    }
-    else{
-        printf("Not an Armstrong no");
+    for( int num=1;num<=1000;num++){
+        if(armstrong(num)){
+            printf("%d\n",num);
+        }
     }
 }
